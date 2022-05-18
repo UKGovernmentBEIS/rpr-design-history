f985b0d9f90c5a947602571b90cd052a6418e27f
Bryan Gale <bryan.gale@dxw.com>
2022-05-18 17:52:49 +0100

    Update for deploying to GitHub pages

--- .eleventy.js
+++ .eleventy.js
@@ -46,7 +46,7 @@ module.exports = function (eleventyConfig) {
     markdownTemplateEngine: 'njk',
     dir: {
       input: 'app',
-      output: 'public',
+      output: 'docs',
       layouts: '_layouts',
       includes: '_components'
     },
